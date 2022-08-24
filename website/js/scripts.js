2aa76e4464f8172844c99f0321b1458467c8737c
MarcoLeti <letico.marco@gmail.com>
2022-08-24 18:31:46 +0200

    PV-14 integrate metamask

--- website/js/scripts.js
+++ website/js/scripts.js
@@ -183,3 +183,9 @@ function changeGallery() {
 
 }
 window.onload = changeGallery;
+
+async function connect() {
+    if(typeof window.ethereum !== "undefined") {
+        await ethereum.request({ method: "eth_requestAccounts" });
+    }
+}
---
+++ website/js/smart-contracts.js
@@ -0,0 +1,4 @@
+
+import { ethers } from "../modules/ethers.js";
+
+const provider = new ethers.providers.Web3Provider(window.ethereum);
